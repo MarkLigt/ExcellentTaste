926fac1358c2ddd933d1db5cb5ac32e974f0414e
Mark Ligt <markligt@hotmail.nl>
2018-11-15 12:51:15 +0100

    test2
    test3

--- index.php
+++ index.php
@@ -1,15 +1,6 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-    <meta name="viewport" content="width=device-width">
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title>Flowerpower</title>
-</head>
-<link rel="stylesheet" type="text/css" href="style.css">
-<style>body {overflow-x: hidden;}</style>
-<body>
-<div id="header">
-    <div class="headerlogo">
+<head><D<<D<D<D<D<D<D<D
         	<a href="homepage.php">
         		<p>Flowerpower</p>
             </a>
